85e27dfac2c37c860b49c64dd54b91fc35b8e844
Daniel T. Simpson <41841506+DanielTSimpson@users.noreply.github.com>
2024-05-08 22:36:21 -0400

    Added input dxf file, variables from file name, and made the script open FEMM and inport the dxf

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # bldc-to-pyfemm
 A script to interface a DXF of a partial BLDC motor profile into FEMM using pyfemm.
 Current scope is to get the torque from a rotor given a set of instantaneous phase currents.
+DXF naming convention is _"numberslots_numberpoles_numberwindings_statorID_wiredia_toothheight_toothoverhang_toothwidth_magnetwidth_magnetthk_magnetstatorgap_rotorthk".dxf_
--- bldc-to-pyfemm.py
+++ bldc-to-pyfemm.py
@@ -1,10 +1,27 @@
 # Main script for importing DXF files and getting torque values in FEMM
 import femm
+
+FILENAME = "36_38_20_122_0.9_2_1.5_3_10_3_0.75_3.dxf"
+properties = FILENAME.split("_")
+SLOTS = properties[0]
+POLES = properties[1]
+NUMBER_WINDINGS = properties[2]
+STATOR_ID = properties[3]
+WIRE_DIA = properties[4]
+TOOTH_HEIGHT = properties[5]
+TOOTH_OVERHANG = properties[6]
+MAGNET_WIDTH = properties[7]
+MAGNET_THK = properties[8]
+MAGNET_STATOR_GAP = properties[9]
+ROTOR_THK = properties[10]
+
 femm.openfemm()
 femm.newdocument(0)
-femm.mi_probdef(0,'millimeters','planar',1E-8,20,20,(0))
-femm.mi_drawrectangle(0,0,10,10)
-femm.mi_addblocklabel(5,5)
-femm.mi_selectlabel(4,5)
-input(0)
+femm.mi_probdef(0, 'millimeters', 'planar', 1E-8, 20, 20, 0)
+femm.mi_readdxf("Input DXFs/" + FILENAME)
+femm.mi_zoomnatural()
 
+# femm.mi_addblocklabel(5,5)
+# femm.mi_selectlabel(4,5)
+
+input(0)
