e63111541d92e4dd8359255b1e2ad4f2724cf5f8
Daniel T. Simpson <41841506+DanielTSimpson@users.noreply.github.com>
2024-05-08 21:32:35 -0400

    Testing how to commit

--- bldc-to-pyfemm.py
+++ bldc-to-pyfemm.py
@@ -1,1 +1,10 @@
+# Main script for importing DXF files and getting torque values in FEMM
 import femm
+femm.openfemm()
+femm.newdocument(0)
+femm.mi_probdef(0,'millimeters','planar',1E-8,20,20,(0))
+femm.mi_drawrectangle(0,0,10,10)
+femm.mi_addblocklabel(5,5)
+femm.mi_selectlabel(4,5)
+input(0)
+
