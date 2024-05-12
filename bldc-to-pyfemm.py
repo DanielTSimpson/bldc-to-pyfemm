2cabeee6face852fcb45780532e03f9c7279dab5
Daniel T. Simpson <41841506+DanielTSimpson@users.noreply.github.com>
2024-05-12 00:16:41 -0400

    Updated circ_pattern function with simpler, more general solution. Fixed bug with off-center pattern. Added position tracking lists for all rotors and coils.

--- bldc-to-pyfemm.py
+++ bldc-to-pyfemm.py
@@ -20,21 +20,24 @@ MAGNET_STATOR_GAP = float(properties[11])
 ROTOR_THK = float(properties[12])
 
 
-def circ_pattern(start_pos: list, angle: float, num_copies: int, direction: int):
+def circ_pattern(start_pos: list, angle: float, num_copies: int, direction: int, save_array: list = None):
     """
     Adds block labels in a circular pattern
-    :param start_pos: the point from the center of the motor. the center of the motor is the center of rotation
+    :param start_pos: a point [x,y] in space. [0,0] is the center of rotation
     :param angle: the angle between copies
     :param num_copies: the number of copies
     :param direction: the direction of the circular pattern. +1 for clockwise, -1 for counterclockwise
+    :param save_array: optional list to save block label positions to
     """
     radius = math.pow(math.pow(start_pos[0], 2) + math.pow(start_pos[1], 2), 0.5)
+    phi_0 = math.acos(start_pos[0] / radius)
+    print(type(save_array))
     for i in range(1, num_copies + 1):
-        dx = radius * direction * math.sin(i * angle)
-        cot = math.tan(math.pi / 2 - i * angle / 2) and (1 / math.tan(math.pi / 2 - i * angle / 2)) or 2 / math.sin(
-            i * angle)
-        dy = radius * math.sin(i * angle) * cot
-        femm.mi_addblocklabel(start_pos[0] + dx, start_pos[1] - dy)
+        x = radius * math.cos(phi_0 + direction * i * angle)
+        y = radius * math.sin(phi_0 + direction * i * angle)
+        femm.mi_addblocklabel(x, y)
+        if save_array:
+            save_array.append([x, y])
 
 
 # Initialize FEMM Magnetic Problem
@@ -45,44 +48,45 @@ femm.mi_readdxf("Input DXFs/" + FILENAME + ".dxf")
 femm.mi_zoomnatural()
 
 # Define the position of the stator, rotor, magnet, and coils
+air_position = [0, 2 * STATOR_ID - 1]
 stator_position = [0, STATOR_ID / 2 + STATOR_BASE / 2]
 
 rotor_position = [0,
                   STATOR_ID / 2 + STATOR_BASE + float(NUMBER_WINDINGS) * WIRE_DIA + TOOTH_HEIGHT + MAGNET_STATOR_GAP
                   + MAGNET_THK + ROTOR_THK / 2]
 
-magnet_position = [0,
-                   STATOR_ID / 2 + STATOR_BASE + float(NUMBER_WINDINGS) * WIRE_DIA + TOOTH_HEIGHT + MAGNET_STATOR_GAP
-                   + MAGNET_THK / 2]
+magnet_positions = [[0,
+                     STATOR_ID / 2 + STATOR_BASE + float(NUMBER_WINDINGS) * WIRE_DIA + TOOTH_HEIGHT + MAGNET_STATOR_GAP
+                     + MAGNET_THK / 2]]
+
 magnet_angle = 2 * math.pi / POLES
 
-tooth_r = STATOR_ID / 2 + STATOR_BASE + float(NUMBER_WINDINGS) * WIRE_DIA / 2
-tooth_angle = 2 * math.pi / SLOTS
-tooth_position = [tooth_r * math.sin(tooth_angle / 2), tooth_r * math.cos(tooth_angle / 2)]
-coil1_position = [tooth_position[0] - (TOOTH_WIDTH + WIRE_DIA) / 2 * math.cos(tooth_angle / 2),
-                  tooth_position[1] + (TOOTH_WIDTH + WIRE_DIA) / 2 * math.sin(tooth_angle / 2)]
-coil2_position = [tooth_position[0] + (TOOTH_WIDTH + WIRE_DIA) / 2 * math.cos(tooth_angle / 2),
-                  tooth_position[1] - (TOOTH_WIDTH + WIRE_DIA) / 2 * math.sin(tooth_angle / 2)]
+slot_r = STATOR_ID / 2 + STATOR_BASE + float(NUMBER_WINDINGS) * WIRE_DIA / 2
+slot_angle = 2 * math.pi / SLOTS
+slot_position = [slot_r * math.sin(5 * slot_angle / 2), slot_r * math.cos(5 * slot_angle / 2)]
+
+coil1_positions = [[slot_position[0] - (TOOTH_WIDTH + WIRE_DIA) / 2 * math.cos(5 * slot_angle / 2),
+                    slot_position[1] + (TOOTH_WIDTH + WIRE_DIA) / 2 * math.sin(5 * slot_angle / 2)]]
+coil2_positions = [[slot_position[0] + (TOOTH_WIDTH + WIRE_DIA) / 2 * math.cos(5 * slot_angle / 2),
+                   slot_position[1] - (TOOTH_WIDTH + WIRE_DIA) / 2 * math.sin(5 * slot_angle / 2)]]
+
+# Add air position
+femm.mi_addblocklabel(air_position[0], air_position[1])
 
 # Add stator base and rotor positions
 femm.mi_addblocklabel(stator_position[0], stator_position[1])
 femm.mi_addblocklabel(rotor_position[0], rotor_position[1])
 
 # Add magnet positions
-femm.mi_addblocklabel(magnet_position[0], magnet_position[1])
-circ_pattern(magnet_position, magnet_angle, int(POLES / 2), 1)
-circ_pattern(magnet_position, magnet_angle, int(POLES / 2), -1)
+femm.mi_addblocklabel(magnet_positions[0][0], magnet_positions[0][1])
+circ_pattern([magnet_positions[0][0], magnet_positions[0][1]], magnet_angle, int(POLES) - 1, 1, magnet_positions)
 
 # Add coil1 positions
-femm.mi_addblocklabel(coil1_position[0], coil1_position[1])
-circ_pattern(coil1_position, tooth_angle, int(SLOTS / 2), 1)
-circ_pattern(coil1_position, tooth_angle, int(SLOTS / 2), -1)
+femm.mi_addblocklabel(coil1_positions[0][0], coil1_positions[0][1])
+circ_pattern([coil1_positions[0][0], coil1_positions[0][1]], slot_angle, 5, 1, coil1_positions)
 
 # Add coil2 positions
-femm.mi_addblocklabel(coil2_position[0], coil2_position[1])
-circ_pattern(coil2_position, tooth_angle, int(SLOTS / 2), 1)
-circ_pattern(coil2_position, tooth_angle, int(SLOTS / 2), -1)
-
-# femm.mi_selectlabel(4,5)
+femm.mi_addblocklabel(coil2_positions[0][0], coil2_positions[0][1])
+circ_pattern([coil2_positions[0][0], coil2_positions[0][1]], slot_angle, 5, 1, coil2_positions)
 
 input(0)
